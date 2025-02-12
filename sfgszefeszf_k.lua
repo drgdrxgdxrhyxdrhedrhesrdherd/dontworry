4e3e494359015f27b9d729e9ddb845572be37552
drgdrxgdxrhyxdrhedrhesrdherd <z64875421@gmail.com>
2025-02-12 14:41:02 +0300

    Update sfgszefeszf_k.lua

--- sfgszefeszf_k.lua
+++ sfgszefeszf_k.lua
@@ -9,7 +9,7 @@ local device = ""
 local link = "https://ify.ac/1YEI"
 
 local adminkey = "sosal?"
-local KEY = "ldfgdr"
+local KEY = "999999"
 
 local GUI = Instance.new("ScreenGui")
 local s, a = pcall(function() GUI.Parent = game.CoreGui end) if not s then GUI.Parent = game.Players.LocalPlayer.PlayerGui end
